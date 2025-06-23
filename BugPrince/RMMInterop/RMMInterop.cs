d8491b287ebc775bae3d56f8e17871c0d7aa9fac
dplochcoder <daniel.ploch.coder@gmail.com>
2025-06-22 21:56:34 -0400

    Use new RMM hooks

--- BugPrince/RMMInterop/RMMInterop.cs
+++ BugPrince/RMMInterop/RMMInterop.cs
@@ -1,9 +1,4 @@
 ﻿using Modding;
-using RandoMapCore.Data;
-using RandomizerMod.RandomizerData;
-using System;
-using System.Collections.Generic;
-using System.Reflection;
 
 namespace BugPrince.RMMInterop;
 
@@ -11,42 +6,11 @@ internal static class RMMInterop
 {
     internal static void MaybeUpdateRandoMapMod()
     {
-        if (ModHooks.GetMod("RandoMapMod") is null) return;
-        UpdateRandoMapModImpl();
+        if (ModHooks.GetMod("RandoMapCoreMod") is not null) UpdateRandoMapCoreModImpl();
+        if (ModHooks.GetMod("RandoMapMod") is not null) UpdateRandoMapModImpl();
     }
 
-    private static void UpdateRandoMapModImpl() => RMMInteropImpl.UpdateRmmDataModule();
-}
+    private static void UpdateRandoMapCoreModImpl() => RandoMapCore.RandoMapCoreMod.RebuildModules();
 
-// Rando map mod assumes that randomized transition placements only change on entering the game.
-internal static class RMMInteropImpl
-{
-    private static readonly Type moduleType = Type.GetType("RandoMapMod.RmmDataModule, RandoMapMod");
-    private static readonly FieldInfo randomizedTransitionsField = moduleType.GetField("_randomizedTransitions", BindingFlags.NonPublic | BindingFlags.Static);
-    private static readonly FieldInfo randomizedTransitionPlacementsField = moduleType.GetField("_randomizedTransitionPlacements", BindingFlags.NonPublic | BindingFlags.Static);
-
-    private static RmcTransitionDef ConvertTransitionDef(TransitionDef def) => new()
-    {
-        SceneName = def.SceneName,
-        DoorName = def.DoorName,
-        VanillaTarget = def.VanillaTarget,
-    };
-
-    internal static void UpdateRmmDataModule()
-    {
-        if (randomizedTransitionsField.GetValue(null) is not Dictionary<string, RmcTransitionDef> randomizedTransitions) return;
-        if (randomizedTransitionPlacementsField.GetValue(null) is not Dictionary<RmcTransitionDef, RmcTransitionDef> randomizedTransitionPlacements) return;
-
-        randomizedTransitions.Clear();
-        randomizedTransitionPlacements.Clear();
-        foreach (var placement in RandomizerMod.RandomizerMod.RS.Context.transitionPlacements)
-        {
-            var sourceDef = ConvertTransitionDef(placement.Source.TransitionDef);
-            var targetDef = ConvertTransitionDef(placement.Target.TransitionDef);
-
-            randomizedTransitions[placement.Source.Name] = sourceDef;
-            randomizedTransitions[placement.Target.Name] = targetDef;
-            randomizedTransitionPlacements[sourceDef] = targetDef;
-        }
-    }
+    private static void UpdateRandoMapModImpl() => RandoMapMod.RandoMapMod.RebuildModules();
 }
